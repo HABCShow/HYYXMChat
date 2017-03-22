d4c39b3311a0014b42970e8d39a3c01aed747704
ABC_Show <361569679@qq.com>
2017-03-22 15:54:59 +0800

    项目创建

---
+++ HYYXMChat/HYYXMChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HYYXMChat
+//
+//  Created by xuchunlei on 2017/3/22.
+//  Copyright © 2017年 abc_show. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HYYXMChat/HYYXMChat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HYYXMChat
+//
+//  Created by xuchunlei on 2017/3/22.
+//  Copyright © 2017年 abc_show. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
